5ef6a2bbcf451c993a1d4810cd8aa9a3636c2826
Mistxs <96961160+Mistxs@users.noreply.github.com>
2022-05-06 02:34:41 +0300

    Update m_lib.js

--- m_lib.js
+++ m_lib.js
@@ -1,1 +1,74 @@
-alert ("ку-ку")
+let parent = document.querySelector('.page-container');
+let before = document.querySelector('.main-content');
+
+let p = document.createElement('div');
+p.id = "backtop";
+p.innerHTML = '&uarr;';
+
+parent.insertBefore(p, before);
+
+
+
+/* begin Up-Down button  */
+(function() {
+  'use strict';
+
+  var upDownBtn = document.querySelector('#backtop');
+  var check;
+
+  function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      upDownBtn.classList.add('up_down_btn-show');
+      upDownBtn.innerHTML = '&uarr;';
+      upDownBtn.setAttribute('title', 'Наверх');
+      check = false;
+    }
+    if (scrolled === 0) {
+      upDownBtn.innerHTML = '&darr;';
+      upDownBtn.setAttribute('title', 'Вниз');
+      check = true;
+    }
+  }
+
+  function backToTop() {
+    upDownBtn.classList.add('up_down_btn-disabled');
+    if (!check) {
+      (function goTop() {
+
+        if (window.pageYOffset !== 0) {
+          window.scrollBy(0, -80);
+          setTimeout(goTop, 10);
+        } else {
+          upDownBtn.classList.remove('up_down_btn-disabled');
+        }
+
+      })();
+      return;
+
+    } else if (check) {
+      (function goBottom() {
+        var match = Math.ceil(window.pageYOffset + document.documentElement.clientHeight);
+
+        if (match != document.documentElement.scrollHeight) {
+          window.scrollBy(0, 80);
+          setTimeout(goBottom, 10);
+        } else {
+          upDownBtn.classList.remove('up_down_btn-disabled');
+        }
+
+      })();
+      return;
+    }
+
+  }
+
+  window.addEventListener('scroll', trackScroll);
+  upDownBtn.addEventListener('click', backToTop);
+})();
+/* end Up-Down button  */
+
+
+
